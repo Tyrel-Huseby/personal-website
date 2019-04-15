5e1811dc3e2a3751f70219a0e4f52428a1c2de4d
Tyrel Huseby <thuseby.business@gmail.com>
2019-04-15 10:45:27 -0600

    added github links

--- javascript/games.js
+++ javascript/games.js
@@ -1,5 +1,17 @@
 var openGame;
 
 function game(dir) {
-  openGame = window.open(`games/${dir}/${dir}.html`, `${dir}Window`);
+  openGame = window.open(`games/${dir}/${dir}.html`);
+}
+
+function pythonGame(name) {
+  if (name == 'five card draw') {
+    openGame = window.open(`https://github.com/Tyrel-Huseby/python-games/blob/master/fiveCardDraw.py`);
+  }
+  else if (name == 'go fish') {
+    openGame = window.open(`https://github.com/Tyrel-Huseby/python-games/blob/master/goFish.py`);
+  }
+  else if (name == 'r.p.s.l.s') {
+    openGame = window.open(`https://github.com/Tyrel-Huseby/python-games/blob/master/rockPaperScissorsLizardSpock.py`);
+  }
 }
